13e01ad5747fa364d604bcf856b7e1a06269538c
MathiasDWaterlaw <mathiasdellosa@gmail.com>
2024-03-13 03:41:50 +0100

    added js for filling the game grid

--- js/script.js
+++ js/script.js
@@ -7,10 +7,15 @@ const resetBtn = document.getElementById('reset-btn');
 const dialog = document.getElementById('dialog-section');
 const confirmName = document.getElementById('confirm-players-name');
 const closeDialog = document.getElementById('close-dialog');
-const firstPlayerName = document.getElementById('p1-name');
-const secondPlayerName = document.getElementById('p2-name');
+
 const firstNameInput = document.getElementById('input-player-1');
 const secondNameInput = document.getElementById('input-player-2');
+
+const firstPlayerName = document.getElementById('p1-name');
+const secondPlayerName = document.getElementById('p2-name');
+
+const gridBoxes = document.querySelectorAll('.grid-box');
+let excludedGridBoxes = [];
 
 function Player(name, symbol) {
     this.name = name;
@@ -18,14 +23,39 @@ function Player(name, symbol) {
     this.active = false;
 }
 
-function startGame(playerNames){
-    const [firstPlayerName, secondPlayerName] = playerNames;
-    const firstPlayer = new Player(firstPlayerName, 'X');
-    const secondPlayer = new Player(secondPlayerName, 'O');
+const firstPlayer = new Player('Player 1', 'X');
+const secondPlayer = new Player('Player 2', 'O');
 
-    console.log(firstPlayer.name, firstPlayer.symbol)
+function startGame(players){
+    const [firstPlayer, secondPlayer] = players;
+
+    firstPlayer.active = true;
+
+    gridBoxes.forEach((box) => 
+        box.addEventListener('click', () => {
+
+            if (!excludedGridBoxes.includes(box.id) && firstPlayer.active) {
+                box.innerHTML = `<h1>${firstPlayer.symbol}</h1>`;
+                firstPlayer.active = false;
+                secondPlayer.active = true;
+                
+            } else if (!excludedGridBoxes.includes(box.id) && secondPlayer.active) {
+                box.innerHTML = `<h1>${secondPlayer.symbol}</h1>`;
+                firstPlayer.active = true;
+                secondPlayer.active = false;
+            }
+            excludedGridBoxes.push(box.id);
+        })
+    );
 }
 
+function clearBoard() {
+    gridBoxes.forEach((box) => box.innerHTML = '');
+    excludedGridBoxes = [];
+    firstPlayer.active = true;
+}
+
+// buttons listeners and functions
 
 startBtn.addEventListener('click', () => {
     btnContainer.style.display = 'flex';
@@ -33,17 +63,24 @@ startBtn.addEventListener('click', () => {
     dialog.showModal();
 });
 
+restartBtn.addEventListener('click', () => {
+    clearBoard();
+})
+
 resetBtn.addEventListener('click', () => {
     firstPlayerName.textContent = 'Player 1';
     secondPlayerName.textContent = 'Player 2';
     btnContainer.style.display = 'none';
     startBtn.classList.toggle('hidden');
+    clearBoard()
 });
 
 confirmName.addEventListener('click', () => {
     firstPlayerName.textContent = firstNameInput.value;
     secondPlayerName.textContent = secondNameInput.value;
-    startGame([firstNameInput.value, secondNameInput.value]);
+    firstPlayer.name = firstNameInput.value;
+    secondPlayer.name = secondNameInput.value;
+    startGame([firstPlayer, secondPlayer]);
     dialog.close();
 });
 
